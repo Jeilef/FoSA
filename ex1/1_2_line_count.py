a2875b0cba5080b89563b936dfa3116e2af37122
pawelsoundso <pawel.gloeckner@student.hpi.uni-potsdam.de>
2019-04-17 16:18:28 +0200

    solve 1.4.4

--- ex1/1_2_line_count.py
+++ ex1/1_2_line_count.py
@@ -3,7 +3,7 @@ from os import walk
 
 for dirpath, dnames, fnames in walk("./qtbase"):
     for filename in fnames:
-        line_count = 1 #for the eof line break
+        line_count = 0
         for line in open(dirpath + "/" +  filename, encoding='utf8', errors='replace'):
             line_count+=1
         print(filename + ';' + str(line_count))
--- ex1/1_4_repository_mining.sh
+++ ex1/1_4_repository_mining.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+
 git log --format="%ae" | sort | uniq | wc -l | awk '{print "Number of authors: "$1}'
 git log --format="%ar;/%an <%ae>" --since="1 year ago" | awk -F ";" '{print $2}' | sort | uniq -c | awk -F "/" 'max==""||$1>max {max=$1;value=$2} END {print "Most active author within last year: "value}'
 git log --format="%ct-%cr" | tail -n 1 | awk -F "-" -v date="$(date +%s)" '{printf "Time range of development: %.0f days (started " $2")\n", ((date - $1) / (24 * 3600))}'
+git log --max-parents=1 --format="%s"| grep -E "fix|rewrite|doc|test|improve" | wc -l | awk -v total=$(git log --max-parents=1 --format="%s" | wc -l) '{printf "Share of maintenance: %.1f%\n", $1 / total * 100}'
 
