6c7994399ce0c013efdf23fc89b5cb044f68fba4
justus.eilers <justus.eilers@student.hpi.de>
2019-04-17 12:32:12 +0200

    final script

--- ex1/1_3_author_consolidation.py
+++ ex1/1_3_author_consolidation.py
@@ -6,18 +6,10 @@ for line in sys.stdin:
     author = line.split(";")[0]
     mail = line.split(";")[1].split("\n")[0]
     if author in authors:
-        print(author)
-        print(mail)
-        print(authors.get(author))
         if mail not in authors.get(author):
-            authors[author] = authors[author].append(mail)
+            authors[author].append(mail)
     else:
-        authors[author] = [].append(mail)
-        if author == "Allan Sandfeld Jensen":
-            print(authors.get("Allan Sandfeld Jensen"))
-
-
-print(authors.get("Allan Sandfeld Jensen"))
+        authors[author] = [mail]
 
 for k in authors:
     print(k, " ".join(authors.get(k)))
