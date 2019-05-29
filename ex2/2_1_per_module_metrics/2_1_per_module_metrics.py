63a69ec08e50eb1a25699a9979f7a1c3ee2436d9
justus.eilers <justus.eilers@student.hpi.de>
2019-05-29 15:34:53 +0200

    fixes

--- ex2/2_1_per_module_metrics/2_1_per_module_metrics.py
+++ ex2/2_1_per_module_metrics/2_1_per_module_metrics.py
@@ -10,17 +10,33 @@ def is_cpp_file(file_name):
     return end in ["h", "cpp", "hpp", "c"]
 
 
-file_metrics = {}
+def transform_file_paths(output, lookup):
+    transformed_files = []
+    for o in output:
+        if "#" not in o:
+            new_file = os.path.realpath(o)
+            if new_file in lookup:
+                transformed_files.append(new_file)
+        else:
+            transformed_files.append(o)
+    return transformed_files
 
+
+file_metrics = {}
+file_path_start = ""
 for root, dirs, files in os.walk("."):
+    file_path_start = root[:2]
     for name in files:
         if is_cpp_file(name):
-            file_metrics[os.path.join(root, name)] = [365, 0, 0, 0, 0]
+            file_metrics[os.path.realpath(os.path.join(root,name))] = [365, 0, 0, 0, 0]
 
 
 # Mean Time Between Changes
 history = subprocess.run(["git", "log", "--format=#%ct", "--name-only", "--since=1 year ago "], stdout=subprocess.PIPE)
-output = list(filter(lambda x: is_cpp_file(x) and "./" + x in file_metrics, history.stdout.decode('UTF8').split("\n")))
+history = history.stdout.decode('UTF8').split("\n")
+output = list(filter(lambda x: is_cpp_file(x) or '#' in x, history))
+output = transform_file_paths(output, file_metrics)
+
 timestamp_dict ={}
 current_time = 0
 
@@ -37,14 +53,21 @@ for files, timestamps in timestamp_dict.items():
         for t_index in range(len(timestamps) - 1):
             diff = timestamps[t_index] - timestamps[t_index + 1]
             differences.append(diff)
-        file_metrics["./" + files][0] = int(np.mean(differences) / 60 / 60 / 24)
+        file_metrics[files][0] = int(np.mean(differences) / 60 / 60 / 24)
     else:
         # if file only was changed once we assume MTCB to be the difference between today and the one given date
-        file_metrics["./" + files][0] = int((time.time()/1000 - timestamps[0]) / 60 / 60 / 24)
+        file_metrics[files][0] = int((time.time()/1000 - timestamps[0]) / 60 / 60 / 24)
+
+for i in file_metrics:
+    if "bloom.cpp" in i:
+        print(i, file_metrics[i])
 
 # NUMBER OF AUTHORS
 history = subprocess.run(["git", "log", "--format=#%ae", "--name-only", "--since=1 year ago "], stdout=subprocess.PIPE)
-output = list(filter(lambda x: is_cpp_file(x) and "./" + x in file_metrics, history.stdout.decode('UTF8').split("\n")))
+history = history.stdout.decode('UTF8').split("\n")
+output = list(filter(lambda x: is_cpp_file(x) or '#' in x, history))
+output = transform_file_paths(output, file_metrics)
+
 
 author_dict = {}
     
@@ -55,7 +78,7 @@ for line in output:
         author_dict.setdefault(line, set()).add(current_time)
 
 for file, authors in author_dict.items():
-    file_metrics['./' + file][1] = len(authors)
+    file_metrics[file][1] = len(authors)
 
 
 # BOTCH FACTOR
